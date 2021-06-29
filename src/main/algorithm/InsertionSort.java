15f4e5f75f37fbecb84cbcb46718c18d5260fda0
hanjinfeng0309 <2015102239@nit.edu.cn>
2021-06-29 20:37:58 +0800

    优化内层循环

--- src/main/algorithm/InsertionSort.java
+++ src/main/algorithm/InsertionSort.java
@@ -11,10 +11,8 @@ public class InsertionSort {
         // arr[i...n) 未排序
         for (int i = 0; i < arr.length; i++) {
             // 将arr[i]插入到合适的位置，保持arr[0...i) 已排序
-            for (int j = i; j > 0; j--) {
-                if (arr[j].compareTo(arr[j - 1]) < 0) {
-                    SortingHelper.swap(arr, j, j - 1);
-                } else break;
+            for (int j = i; j > 0 && arr[j].compareTo(arr[j - 1]) < 0; j--) {
+                SortingHelper.swap(arr, j, j - 1);
             }
         }
     }
